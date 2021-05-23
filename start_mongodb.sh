d52f4da00a5c583ff798f84da5049513080a4e47
Timmehh89 <timm.hengerer@gmx.de>
2021-05-23 07:18:47 +0200

    Bug fixes for external directory execution

--- start_mongodb.sh
+++ start_mongodb.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 source .env
 read -p "Do You want to reset the Database? " -n 1 -r
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo $DIR
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo "Remove Docker Cotainer"
-    docker-compose down
+    docker-compose -f $DIR/docker-compose.yml down
 
-    DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/${LOCAL_DATA_STORAGE}/
-    echo "Check Data Directory $DIR "
-    if [ -d "$DIR" ]; then
+    DIRSTORAGE=$DIR/${LOCAL_DATA_STORAGE}/
+    echo "Check Data Directory $DIRSTORAGE "
+    if [ -d "$DIRSTORAGE" ]; then
         echo "Remove Data from Directory"
-        sudo rm -r "$DIR"
+        sudo rm -r "$DIRSTORAGE"
     fi
     echo "Create Init File"
-    (envsubst <mongo-init-template.txt)>mongo-init.js
+    (envsubst <$DIR/mongo-init-template.txt)>$DIR/mongo-init.js
 fi
 
 echo "Starting Docker"
-docker-compose up
+docker-compose -f $DIR/docker-compose.yml up -d
